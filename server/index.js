02624e057b57108401a44e50810a187e108fe2d0
J. M. Alessi <j.m.alessi27@outlook.com>
2022-05-14 21:15:44 -0400

    connecting server to frontend

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.send('Server');
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'client', 'public', 'index.html'));
+});
+
 app.get('/tweets', (req, res) => {
   res.send('...');
 });
