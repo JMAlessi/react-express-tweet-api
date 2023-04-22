83b1c63bee56ac3f3364d697060c92629f520426
J. A. Software <46306007+J-A-Software@users.noreply.github.com>
2023-04-21 22:55:55 -0400

    refactor index and TweetFeed

--- client/src/components/TweetFeed.js
+++ client/src/components/TweetFeed.js
@@ -11,23 +11,36 @@ const TweetFeed = () => {
 
 	const tweets = useSelector((state) => state.tweets.tweets);
 
-	const tweetData = {
-		id: tweets.data[0].id,
-		text: tweets.data[0].text,
-		username: `@${tweets.includes.users[0].username}`,
-	};
+	let tweetData = null;
+
+	if (
+		tweets &&
+		tweets.data &&
+		tweets.data[0] &&
+		tweets.includes &&
+		tweets.includes.users &&
+		tweets.includes.users[0]
+	) {
+		tweetData = {
+			id: tweets.data[0].id,
+			text: tweets.data[0].text,
+			username: `@${tweets.includes.users[0].username}`,
+		};
+	}
 
 	return (
 		<div className="card my-4">
 			<div className="card-body">
-				<h5 className="card-title">{tweetData.text}</h5>
-				<h6 className="card-subtitle mb-2 text-muted">{tweetData.username}</h6>
-				<a
-					className="btn btn-primary mt-3"
-					href={`https://twitter.com/${tweetData.username}/status/${tweetData.id}`}
-				>
-					<i className="fab fa-twitter"></i> Go to Tweet!
-				</a>
+				<h5 className="card-title">{tweetData?.text}</h5>
+				<h6 className="card-subtitle mb-2 text-muted">{tweetData?.username}</h6>
+				{tweetData && (
+					<a
+						className="btn btn-primary mt-3"
+						href={`https://twitter.com/${tweetData.username}/status/${tweetData.id}`}
+					>
+						<i className="fab fa-twitter"></i> Go to Tweet!
+					</a>
+				)}
 			</div>
 		</div>
 	);
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,14 @@ import './scss/index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { createRoot } from 'react-dom/client';
 
 const root = document.getElementById('root');
-ReactDOM.render(
+const rootElement = createRoot(root);
+rootElement.render(
 	<Provider store={store}>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
-	</Provider>,
-	root
+	</Provider>
 );
