c02f146c9a492770deabae9d50021f29b4dad7f0
J. A. Software <46306007+J-A-Software@users.noreply.github.com>
2023-05-09 21:58:33 -0400

    rm old files

--- client/src/redux/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { takeLatest } from 'redux-saga/effects';
-import { fetchTweets } from './handlers/tweets';
-import { GET_TWEETS_REQUESTED } from '../ducks/tweets';
-
-export function* watcherSaga() {
-	yield takeLatest(GET_TWEETS_REQUESTED, fetchTweets);
-}
--- client/src/redux/sagas/requests/tweets.js
+++
@@ -1,7 +0,0 @@
-export function getApi() {
-	return fetch('http://localhost:3001/tweets')
-		.then((response) => response.json())
-		.catch((error) => {
-			throw error;
-		});
-}
